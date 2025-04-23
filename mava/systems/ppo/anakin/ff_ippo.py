c588cd063649052f37208245d340d6ca12ea2e2c
easonyu0203 <breaking0203@gmail.com>
2025-04-23 17:17:52 +1000

    fix: proper logger yaml

--- mava/systems/ppo/anakin/ff_ippo.py
+++ mava/systems/ppo/anakin/ff_ippo.py
@@ -400,7 +400,6 @@ def learner_setup(
 
 def run_experiment(_config: DictConfig) -> float:
     """Runs experiment."""
-    _config.logger.system_name = "ff_ippo"
     config = copy.deepcopy(_config)
 
     n_devices = len(jax.devices())
